e48887bc881eb33727abcf33ad77de4201a97935
s25380142 <s25380142@ST2-338-17>
2026-03-26 15:20:18 +0300

    Добавьте файлы проекта.

---
+++ RationalLab/Rational.h
@@ -0,0 +1,62 @@
+#pragma once
+
+class Rational
+{
+public:
+
+    Rational();
+    Rational(float value);
+    void Ratgcd(Rational& gcdval);
+    void SetVal(float value);
+    float GetVal() const;
+    void PrValue() const;
+
+    // Arithmetic Operators
+    Rational operator +(const Rational& x);
+    Rational operator -(const Rational& x);
+    Rational operator *(const Rational& x);
+    Rational operator /(const Rational& x);
+
+    // Assignment Operators
+    Rational& operator =(const Rational& x);
+    Rational& operator +=(const Rational& x);
+    Rational& operator -=(const Rational& x);
+    Rational& operator *=(const Rational& x);
+    Rational& operator /=(const Rational& x);
+
+    // Operators unar
+    Rational& operator ++();
+    Rational operator ++(int);
+    Rational& operator --();
+    Rational operator --(int);
+
+
+    //Relational Operators
+    bool operator==(const Rational& x);
+    bool operator!=(const Rational& x);
+    bool operator>(const Rational& x);
+    bool operator<(const Rational& x);
+    bool operator>=(const Rational& x);
+    bool operator<= (const Rational& x);
+
+    //Logical Operators
+    bool operator&& (const Rational& x);
+    bool operator|| (const Rational& x);
+    bool operator! ();
+
+    //Bitwise Operators
+    Rational operator& (const Rational& x);
+    Rational operator| (const Rational& x);
+    Rational operator^ (const Rational& x);
+    Rational operator<< (const Rational& x);
+    Rational operator>> (const Rational& x);
+    Rational operator~ ();
+
+    //Casting Operators
+    operator double() const;
+    operator float() const;
+    operator int() const;
+private:
+    int64_t num;
+    uint64_t del;
+};
